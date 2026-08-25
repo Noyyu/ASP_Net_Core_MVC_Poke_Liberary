3d8da05f984ca51a06c0f55329b2e00f5be744c9
User <User@DESKTOP-JA8SIQ5>
2026-08-25 09:21:19 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP_Net_Core_MVC_Liberary.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
